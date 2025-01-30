c6cacbb67c6d35a68b0884a789c0f2224674946d
t184914 <tor.tsn@gmail.com>
2025-01-30 18:36:27 +0100

    Prøvd på test

--- oblig1/src/test/FilmarkivTest.java
+++ oblig1/src/test/FilmarkivTest.java
@@ -1,5 +1,14 @@
 package test;
 
-public class FilmarkivTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+class FilmarkivTest {
+
+	@Test
+	void testFilmarkiv2() {
+		fail("Not yet implemented");
+	}
+
+	
 }
